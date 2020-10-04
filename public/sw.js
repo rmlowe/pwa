7e47c987950e8a2c2cd43a67ccdde2c0cacd7f02
Robert Lowe <robertmlowe@rmlowe.com>
2020-10-04 13:50:41 +0800

    Adding & Retrieving Multiple Files (to/ from Cache)

--- public/sw.js
+++ public/sw.js
@@ -5,6 +5,8 @@ self.addEventListener('install', function (event) {
     caches.open('static')
       .then(function (cache) {
         console.log('[Service Worker] Precaching App Shell');
+        cache.add('/');
+        cache.add('/index.html');
         cache.add('/src/js/app.js');
       })
   )
