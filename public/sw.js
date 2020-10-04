e167a9920dd51ac7f3382eac44dc3191dfde9396
Robert Lowe <robertmlowe@rmlowe.com>
2020-10-04 23:04:46 +0800

    Optimizing Cache Management

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,11 @@
 
+var CACHE_STATIC_NAME = 'static-v4';
+var CACHE_DYNAMIC_NAME = 'dynamic-v2';
+
 self.addEventListener('install', function (event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
   event.waitUntil(
-    caches.open('static-v3')
+    caches.open(CACHE_STATIC_NAME)
       .then(function (cache) {
         console.log('[Service Worker] Precaching App Shell');
         cache.addAll([
@@ -30,7 +33,7 @@ self.addEventListener('activate', function (event) {
     caches.keys()
       .then(function (keyList) {
         return Promise.all(keyList.map(function (key) {
-          if (key !== 'static-v3' && key !== 'dynamic') {
+          if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
             console.log('[Service Worker] Removing old cache.', key);
             return caches.delete(key);
           }
@@ -49,7 +52,7 @@ self.addEventListener('fetch', function (event) {
         } else {
           return fetch(event.request)
             .then(function (res) {
-              return caches.open('dynamic')
+              return caches.open(CACHE_DYNAMIC_NAME)
                 .then(function (cache) {
                   cache.put(event.request.url, res.clone());
                   return res;
