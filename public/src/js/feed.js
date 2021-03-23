020b5c83163739930811353287c9e0635cf6b897
Robert Lowe <robertmlowe@rmlowe.com>
2021-03-23 21:11:45 +0800

    Post Request and Cache API

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -75,10 +75,19 @@ function createCard() {
   sharedMomentsArea.appendChild(cardWrapper);
 }
 
-var url = 'https://httpbin.org/get';
+var url = 'https://httpbin.org/post';
 var networkDataReceived = false;
 
-fetch(url)
+fetch(url, {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  },
+  body: JSON.stringify({
+    message: 'Some message'
+  })
+})
   .then(function (res) {
     return res.json();
   })
